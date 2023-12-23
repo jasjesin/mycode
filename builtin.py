2b549045d637070a7bca30eba80ef94339627e59
Rinky <sunnycatchme@gmail.com>
2023-12-23 00:16:42 -0800

    testing upload

--- builtin.py
+++
@@ -1,8 +0,0 @@
-import os      # to fetch OS-specific details
-import logging # to log error or debug msgs for testing
-
-print(os.name)
-
-logger = logging.getLogger("MAIN")
-logger.error("Error happened in builtin app")
-
